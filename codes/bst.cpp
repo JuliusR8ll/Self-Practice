9426f321483d9f43bcfd0fc5f4be027349f32c9f
ry444 <wilsonjulius69@gmail.com>
2024-02-02 01:44:14 +0530

    insertion

--- codes/bst.cpp
+++ codes/bst.cpp
@@ -8,6 +8,12 @@ struct Node{
         left = right = NULL;
     }
 };
+void inorder(Node* root){
+    if(root == NULL)return;
+    inorder(root->left);
+    cout << root ->key <<" ";
+    inorder(root->right);
+}
 bool find(Node* root,int n){
     if(root == NULL)return 0;
     else if(root ->key == n)return 1;
@@ -23,6 +29,37 @@ bool itrSearch(Node* root,int n){
     }
     return 0;
 }
+// Node* insert(Node* root,int x){
+//     if(root == NULL){
+//         Node* curr = new Node(x);
+//         return curr;
+//     } 
+//     if(root->key == x)return root;
+//     else if(root->key > x){
+//         if(root ->left == NULL){
+//             root->left = new Node(x);
+//             return root;
+//         }
+//         else insert(root->left,x);
+//         }
+//     else{
+//         if(root ->right == NULL){
+//             root->right = new Node(x);
+//             return root;
+//         }
+//         else insert(root->right,x);
+//         }
+// }
+Node* insert(Node* root,int x){
+    if(root == NULL){
+        return new Node(x);
+    }
+    else if(root->key > x){
+        root ->left = insert(root->left,x);
+    }
+    else if(root ->key < x)root -> right = insert(root->right,x);
+    return root;
+}
 int main(){
     Node* root = new Node(30);
     root ->left = new Node(20);
@@ -30,6 +67,9 @@ int main(){
     root ->left->right = new Node(25);
     root ->right = new Node(40);
     root ->right->right = new Node(50);
-    cout << find(root,10);
+    //cout << find(root,10);
+    inorder(root);
+    insert(root,21);
+    inorder(root);
     return 0;
 }
